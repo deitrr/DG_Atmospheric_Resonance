912182c89f6e436866be26dbe73510d4b70877d2
Russell Deitrick <russell.deitrick@gmail.com>
2024-05-02 13:21:16 -0700

    removed clm calculation from land ocean field

--- diurnal_solar_avg.py
+++ diurnal_solar_avg.py
@@ -178,32 +178,36 @@ def dasl(sim, cam_field_name, out_field_name, ndim, recenter=True, geo='all'):
     #below geo options not written to handle 3-D fields
     if geo == 'land':
         #quick and dirty way to mask ocean areas
-        field_reorder *= landfrac_reorder
-        field_reorder[landfrac_reorder==0] = np.nan
-        field_anom_reorder *= landfrac_reorder
-        field_anom_reorder[landfrac_reorder==0] = np.nan
+        field_mean = field_reorder * landfrac_reorder
+        field_meanlandfrac_reorder==0] = np.nan
+        field_anom_mean = field_anom_reorder * landfrac_reorder
+        field_anom_mean[landfrac_reorder==0] = np.nan
+        clm_mean = np.zeros((2,np.int(lmax+1),np.int(lmax+1)))
+        clm_anom = np.zeros((2,np.int(lmax+1),np.int(lmax+1)))
     elif geo == 'ocean':
         #quick and dirty way to mask land areas
-        field_reorder *= (1.0 - landfrac_reorder)
-        field_reorder[landfrac_reorder==1] = np.nan
-        field_anom_reorder *= (1.0 - landfrac_reorder)
-        field_anom_reorder[landfrac_reorder==1] = np.nan
-
-    if ndim == 3:
-        field_mean = np.mean(field_reorder[:ftime,:,:,:], axis=0)
-        field_anom_mean = np.mean(field_anom_reorder[:ftime,:,:,:], axis=0)
-        del field_reorder, field_anom_reorder
-        clm_mean = np.zeros((len(p),2,np.int(lmax+1),np.int(lmax+1)))
-        clm_anom = np.zeros((len(p),2,np.int(lmax+1),np.int(lmax+1)))
-        for ilev in np.arange(len(p)):
-          clm_mean[ilev], chi2 = sh.expand.SHExpandLSQ(field_mean[ilev,:,:].ravel(),lat2d.ravel(),lon2d.ravel(),lmax)
-          clm_anom[ilev], chi2 = sh.expand.SHExpandLSQ(field_anom_mean[ilev,:,:].ravel(),lat2d.ravel(),lon2d.ravel(),lmax)
+        field_mean = field_reorder * (1.0 - landfrac_reorder)
+        field_mean[landfrac_reorder==1] = np.nan
+        field_anom_mean = field_anom_reorder * (1.0 - landfrac_reorder)
+        field_anom_mean[landfrac_reorder==1] = np.nan
+        clm_mean = np.zeros((2,np.int(lmax+1),np.int(lmax+1)))
+        clm_anom = np.zeros((2,np.int(lmax+1),np.int(lmax+1)))
     else:
-        field_mean = np.nanmean(field_reorder[:ftime,:,:], axis=0)
-        field_anom_mean = np.nanmean(field_anom_reorder[:ftime,:,:], axis=0)
-        del field_reorder, field_anom_reorder
-        clm_mean, chi2 = sh.expand.SHExpandLSQ(field_mean.ravel(),lat2d.ravel(),lon2d.ravel(),lmax)
-        clm_anom, chi2 = sh.expand.SHExpandLSQ(field_anom_mean.ravel(),lat2d.ravel(),lon2d.ravel(),lmax)
+        if ndim == 3:
+            field_mean = np.mean(field_reorder[:ftime,:,:,:], axis=0)
+            field_anom_mean = np.mean(field_anom_reorder[:ftime,:,:,:], axis=0)
+            del field_reorder, field_anom_reorder
+            clm_mean = np.zeros((len(p),2,np.int(lmax+1),np.int(lmax+1)))
+            clm_anom = np.zeros((len(p),2,np.int(lmax+1),np.int(lmax+1)))
+            for ilev in np.arange(len(p)):
+              clm_mean[ilev], chi2 = sh.expand.SHExpandLSQ(field_mean[ilev,:,:].ravel(),lat2d.ravel(),lon2d.ravel(),lmax)
+              clm_anom[ilev], chi2 = sh.expand.SHExpandLSQ(field_anom_mean[ilev,:,:].ravel(),lat2d.ravel(),lon2d.ravel(),lmax)
+        else:
+            field_mean = np.nanmean(field_reorder[:ftime,:,:], axis=0)
+            field_anom_mean = np.nanmean(field_anom_reorder[:ftime,:,:], axis=0)
+            del field_reorder, field_anom_reorder
+            clm_mean, chi2 = sh.expand.SHExpandLSQ(field_mean.ravel(),lat2d.ravel(),lon2d.ravel(),lmax)
+            clm_anom, chi2 = sh.expand.SHExpandLSQ(field_anom_mean.ravel(),lat2d.ravel(),lon2d.ravel(),lmax)
 
     np.savez(out_file,lon=lon,lat=lat,rotrate=rotrate,field_anom_mean=field_anom_mean,field_mean=field_mean, clm_mean=clm_mean,clm_anom=clm_anom,lmax=lmax,p=p)
 
